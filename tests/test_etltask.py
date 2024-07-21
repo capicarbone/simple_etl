194ade006b34f48f8dd3f6e0318fcd4d7fdf9905
Carlos Pinelly <cpinelly@gmail.com>
2024-07-21 18:41:38 -0500

    Added test for add_output_column method in ETLTask class.

--- tests/test_etltask.py
+++ tests/test_etltask.py
@@ -1,16 +1,76 @@
 from unittest import TestCase
 
 from simple_etl.loaders import RichConsole, SimpleConsole
+from simple_etl.locators import DictKey
 from simple_etl.readers import DictReader
+from simple_etl.tasks import ETLTask
 from .data import track_data
 from .etl_tasks.driver_times import task
 
 
+class TestMapping:
+    column_1 = DictKey("c1")
+    column_2 = DictKey("c2")
+    column_3 = DictKey("c3")
+
+
 class ETLTaskTestCase(TestCase):
 
-    def test_task_processing(self):
+    def test_add_output_column(self):
 
-        reader = DictReader(track_data)
-        task.reader = reader
+        task = ETLTask(mapping=TestMapping)
 
-        task.load(RichConsole())
+        func_1 = lambda x: x
+        func_2 = lambda x, y: x + y
+        func_3 = lambda x, y, z: x + y + z
+
+        test_input = [
+            {
+                'column_name': 'output_1',
+                'func': func_1,
+                'injects':[TestMapping.column_1]
+            },
+            {
+                'column_name': 'output_2',
+                'func': func_2,
+                'injects':[TestMapping.column_1, TestMapping.column_2]
+            },
+            {
+                'column_name': 'output_3',
+                'func': func_3,
+                'injects':[TestMapping.column_1, TestMapping.column_2, TestMapping.column_3]
+            }
+        ]
+
+        for oc in test_input:
+            task.add_output_column(**oc)
+
+
+        output_columns = task.output_columns
+
+        for input in test_input:
+            column_name = input['column_name']
+
+            self.assertIn(column_name, output_columns)
+            column_specs = output_columns[column_name]
+
+            self.assertEqual(input['func'], column_specs[0])    
+            self.assertEqual(len(input['injects']), len(column_specs[1]))    
+
+            for mapping_name, allocator in column_specs[1]:
+                self.assertIn(allocator, input['injects'])
+                
+                if allocator == TestMapping.column_1:
+                    self.assertEqual(mapping_name, 'column_1')
+                elif allocator == TestMapping.column_2:
+                    self.assertEqual(mapping_name, 'column_2')
+                elif allocator == TestMapping.column_3:
+                    self.assertEqual(mapping_name, 'column_3')
+
+
+    # def test_task_processing(self):
+
+    #     reader = DictReader(track_data)
+    #     task.reader = reader
+
+    #     task.load(RichConsole())
