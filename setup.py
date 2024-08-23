1e4ac378a41c69ee052211d46baacf5fe5efa0c5
Carlos Pinelly <cpinelly@gmail.com>
2024-08-23 15:43:27 -0500

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='simple_etl',
+    version='0.1',
+    packages=['simple_etl'],
+    url='',
+    license='',
+    author='Carlos Pinelly',
+    author_email='',
+    description=''
+)
